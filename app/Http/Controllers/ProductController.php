9b746220c2190115ef2c1707f7e95dd82641d6dd
AdoboFlush <albert.belgera@gmail.com>
2021-09-04 17:09:16 +0800

    init

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+
+    public function getProduct(Request $request)
+    {
+      
+    }
+
+    public function addProduct(Request $request)
+    {
+      
+    }
+
+    public function updateProduct(Request $request)
+    {
+      
+    }
+
+    public function deleteProduct(Request $request)
+    {
+      
+    }
+
+
+}
