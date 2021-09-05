0532d90ac88fefa5e356696e46116d76cb0d66f3
AdoboFlush <albert.belgera@gmail.com>
2021-09-05 19:03:01 +0800

    delete product functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -100,7 +100,7 @@ class ProductController extends Controller
 
                 $product = new Product();
                 $rules = [
-                    'id'=>'required',
+                    'id'=>'required|numeric',
                     'name'=> 'required|unique:products|max:255',
                     'price'=>'required|numeric|between:0.00,99999999.99',
                     'description'=>'required'
@@ -123,8 +123,30 @@ class ProductController extends Controller
             }
             elseif($keyword == 'delete'){
 
-                $id = $request->id;
-                Product::destroy($id);
+                $request_data = json_decode($request->payload, true);
+                if(empty($request_data)){
+                    throw new CustomApiException('No input json payload received');
+                }
+
+                $product = new Product();
+                $rules = [
+                    'id'=>'required|numeric'
+                ];
+            
+                $validator = Validator::make($request_data, $rules);
+                if ($validator->passes()) {
+                    $validated_data = $validator->validated();
+                    $id = $validated_data['id'];
+                    $message = Product::destroy($id);
+                } else {
+                    $error_arr = [];
+                    foreach ($validator->errors()->all() as $message) {
+                        $error_arr[] = $message;
+                    }
+                    $error_msg = json_encode($error_arr);
+                    throw new CustomApiException($error_msg);
+                    $status= 'error';
+                }
 
             }else{
                 throw new CustomApiException('Invalid API Keyword');
